ce33db736ba03a3965396781952674a76a29a410
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2023-09-14 15:17:34 +0200

    first commit

---
+++ PractiseTaskMVC/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PractiseTaskMVC.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string fromUser, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", fromUser, message);
+        }
+    }
+}
